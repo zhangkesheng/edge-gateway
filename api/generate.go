21359dd7ea9b2b95bf6fba2a119d95f7571220b0
zhangkesheng <e@bestzks.com>
2020-11-26 09:51:56 +0800

    :book: Account api

--- api/generate.go
+++ api/generate.go
@@ -1,3 +1,3 @@
-//go:generate protoc -I . --go_out=plugins=grpc:. v1/oauth.proto
-//go:generate protoc -I . --swagger_out=logtostderr=true:. v1/oauth.proto
+//go:generate protoc -I . --go_out=plugins=grpc:. v1/oauth.proto v1/account.proto
+//go:generate protoc -I . --swagger_out=logtostderr=true:. v1/oauth.proto v1/account.proto
 package api
