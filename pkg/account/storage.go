5a060759ddea869541b46f268c599207db507a54
zhangkesheng <1307716475@qq.com>
2020-12-07 15:01:27 +0800

    :sparkles: Implement rds storage

--- pkg/account/storage.go
+++ pkg/account/storage.go
@@ -2,7 +2,11 @@ package account
 
 import (
 	"context"
+	"database/sql"
 	"time"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/pkg/errors"
 )
 
 type User struct {
@@ -27,8 +31,87 @@ type UserAccount struct {
 	RefreshToken string
 	ExpiredAt    int64
 	Raw          interface{}
-	CreateAt     time.Time
+	CreatedAt    time.Time
 	ModifiedAt   time.Time
+}
+
+const (
+	userTable        = "`user`"
+	userAccountTable = "`user_account`"
+)
+
+var (
+	userAccountCols = []string{"id", "user_sub", "open_id", "union_id", "nick", "source", "avatar", "email", "access_token", "scope", "refresh_token", "expired_at", "raw", "created_at", "modified_at"}
+)
+
+type RdsStorage struct {
+	db *sql.DB
+}
+
+func (r *RdsStorage) SaveUser(ctx context.Context, user *User) error {
+	onError := func(err error) error {
+		return errors.Wrap(err, "RdsStorage.SaveUser")
+	}
+
+	builder := sq.Insert(userTable).Columns("sub", "primary_account").Values(user.Sub, user.PrimaryAccount)
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return onError(err)
+	}
+
+	if _, err = r.db.ExecContext(ctx, query, args...); err != nil {
+		return onError(err)
+	}
+
+	return nil
+}
+
+func (r *RdsStorage) SaveUserAccount(ctx context.Context, account *UserAccount) error {
+	onError := func(err error) error {
+		return errors.Wrap(err, "RdsStorage.SaveUserAccount")
+	}
+
+	builder := sq.Replace(userAccountTable).Columns(userAccountCols...).
+		Values(nil, account.UserSub, account.OpenId, account.UnionId, account.Nick, account.Source, account.Avatar, account.Email, account.AccessToken, account.RefreshToken, account.ExpiredAt, account.Raw, nil, nil)
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return onError(err)
+	}
+
+	if _, err = r.db.ExecContext(ctx, query, args...); err != nil {
+		return onError(err)
+	}
+
+	return nil
+}
+
+func (r *RdsStorage) GetUserAccount(ctx context.Context, source, openid string) (*UserAccount, error) {
+	onError := func(err error) (*UserAccount, error) {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, errors.Wrap(err, "RdsStorage.GetUserAccount")
+	}
+
+	builder := sq.Select(userAccountCols...).From(userAccountTable).Where("source=? AND open_id=?", source, openid)
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return onError(err)
+	}
+
+	var account UserAccount
+	if err = r.db.QueryRowContext(ctx, query, args...).
+		Scan(&account.Id, &account.UserSub, &account.OpenId, &account.UnionId, &account.Nick, &account.Source, &account.Avatar,
+			&account.Email, &account.AccessToken, &account.RefreshToken, &account.ExpiredAt, &account.Raw, &account.CreatedAt, &account.ModifiedAt);
+		err != nil {
+		return onError(err)
+	}
+
+	return &account, nil
+}
+
+func newRdsStorage(db *sql.DB) Storage {
+	return &RdsStorage{db: db}
 }
 
 type Storage interface {
